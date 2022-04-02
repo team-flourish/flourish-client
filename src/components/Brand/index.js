40992babf72e1e783af2b84ecbbf421b723e96c1
giacomo <88339527+gi-ba-bu@users.noreply.github.com>
2022-04-02 12:13:56 +0200

    set up first component

---
+++ src/components/Brand/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './hero.css'
+
+const Brand = () => {
+     
+    return (
+        <div className='container'>
+        <h1 className='title'>FLUORISH</h1>
+        </div>
+    );
+}
+
+
+
+
+export default Brand;
