fdc10270919c1b3af728fcb2ccac60c7a0c4d397
giacomo <88339527+gi-ba-bu@users.noreply.github.com>
2022-04-06 01:11:12 +0200

    revert

--- src/pages/A_MainPage/MainPage3.test.js
+++ src/pages/A_MainPage/MainPage3.test.js
@@ -1,51 +1,48 @@
+const mockedUseNavigate = jest.fn();
+
+jest.mock('react-router-dom', () => ({
+    ...jest.requireActual('react-router-dom'),
+   useNavigate: () => mockedUseNavigate, 
+ }));
+
+ let navigate = useNavigate();
+
 import React from 'react';
 import { screen, render, fireEvent } from '@testing-library/react';
 import { default as MainPage } from '.';
-import {MemoryRouter} from 'react-router-dom'
+import {MemoryRouter, useNavigate} from 'react-router-dom'
 import { act } from 'react-dom/test-utils';
 
-const mockedUseNavigate = jest.fn();
+describe('Page A', () => {
+    
+    test.skip('Should navigate from Page A to Page B', () => {
+        const { queryByRole } = render(<MainPage />, {wrapper: MemoryRouter})
+        queryByRole('button', {name: /sign/i})
+        fireEvent.click(queryByRole('button', {name: /sign/i}))
+        //expect(mockedUseNavigate).toHaveBeenCalledTimes(1)
+        // expect(navigate).toHaveBeenCalledWith('/bpage')
+    })
 
-jest.mock('react-router-dom', () => {
-  const actualNav = jest.requireActual('react-router-dom');
-  return {
-    ...actualNav,
-    useNavigation: () => ({
-      navigate: mockedUseNavigate,
-    }),
-  };
-});
 
-describe('Main Page Use Nav', () => {
+    it('it calls a handleSubmit for Log in', () => {
 
-test('it calls a handleSubmit for Log in', () => {
-
-    const mockedUseNavigate = jest.fn();
-
-jest.mock('react-router-dom', () => {
-  const actualNav = jest.requireActual('react-router-dom');
-  return {
-    ...actualNav,
-    useNavigation: () => ({
-      navigate: mockedUseNavigate,
-    }),
-  };
-});
-    // Test first render and componentDidMount
-    act(() => {
-      render(<MainPage />, {wrapper: MemoryRouter})
-    });
-    const button = screen.queryByRole('button', {name: /log/i});
-    expect(button).toBeInTheDocument;
-    //console.log(button)
-  
-    // Test second render and componentDidUpdate
-     await act( async () => {
-         let valueP = button.dispatchEvent(new MouseEvent('click', {bubbles: true}));
-         console.log(valueP)
+    
+        // Test first render and componentDidMount
+        act( () => {
+        render(<MainPage />, {wrapper: MemoryRouter})
         });
-   
-        expect(mockedUseNavigate).toHaveBeenCalledTimes(1)
-  
-  });
-});
+        const button = screen.queryByRole('button', {name: /log/i});
+        expect(button).toBeInTheDocument;
+        //console.log(button)
+
+        // Test second render and componentDidUpdate
+        act( () => {
+              let value = button.dispatchEvent(new MouseEvent('click', {bubbles: true}));
+              console.log(value)
+            });
+
+        expect(useNavigate()).toHaveBeenCalledTimes(1)
+    
+    });
+
+})
