3f3621435aabfeed12d418e4174b19f4e8c19672
rxdvd <rxdvd0@gmail.com>
2022-04-05 19:37:58 +0100

    rating stars functionality

--- src/components/RateProduct/index.js
+++ src/components/RateProduct/index.js
@@ -1,12 +1,48 @@
-import React from "react";
+import React, { useState } from "react";
 
+import starIcon from "../../images/icons/star.svg";
 import "./style.css";
 
-const RateProduct = () => {
+const RateProduct = ({ onChange }) => {
+    const [rating, setRating] = useState(0);
+    const [actualRating, setActualRating] = useState(0);
+
+    const handleStarHover = (e) => {
+        setRating(parseInt(e.target.dataset.stars));
+    };
+
+    const handleStarUnhover = (e) => {
+        setRating(0);
+    };
+
+    const handleClick = (e) => {
+        const selectedRating = parseInt(e.target.dataset.stars);
+        setActualRating(selectedRating);
+        onChange && onChange(selectedRating);
+    };
+
+    let stars = [];
+    for(let i = 0; i < 5; i++){
+        const isFilled = i < (rating || actualRating);
+        stars.push(
+            <img 
+                key={i} 
+                src={starIcon} 
+                className={isFilled ? "lit" : ""} 
+                data-stars={i + 1} 
+                onMouseEnter={handleStarHover} 
+                onMouseOut={handleStarUnhover} 
+                onClick={handleClick}
+            />
+        );
+    }
+
     return (
         <div id="rateProduct">
             <h3>Rate this listing:</h3>
-            <div className="ratingStars">⭐⭐⭐☆☆</div>
+            <div className="ratingStars">
+                {stars}
+            </div>
         </div>
     );
 };
