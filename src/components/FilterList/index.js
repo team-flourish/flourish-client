9ad11dba1861105d8e6c6cf939d610d390c370e2
rxdvd <rxdvd0@gmail.com>
2022-04-06 15:36:22 +0100

    products list functionality

--- src/components/FilterList/index.js
+++ src/components/FilterList/index.js
@@ -1,35 +1,46 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 
-import { categories as categoriesFromFile } from "../../data";
 import "./style.css";
 
-const FilterList = () => {
-    const [categories, setCategories] = useState([]);
+const FilterList = ({ onSelection, categoryData }) => {
+    const [categories, setCategories] = useState(categoryData || []);
+    const [selected, setSelected] = useState([]);
 
-    useEffect(async () => {
-        const response = await fetch(`${API_HOST}/products/categories`);
-        if(response.status === 200) {
-            const data = await response.json();
-            setCategories(data);
+    const handleClick = (e) => {
+        const value = parseInt(e.target.dataset.id);
+        const index = selected.indexOf(value);
+        let newSelected;
+        if(index > -1){
+            newSelected = [...selected.slice(0, index), ...selected.slice(index + 1)];
         } else {
-            setCategories(categoriesFromFile);
+            newSelected = [...selected, value];
         }
-    }, []);
+        setSelected(newSelected);
+        onSelection && onSelection(newSelected);
+    };
 
     return (
         <>
         <h2 className="muted filterListTitle">Filter by food type</h2>
         <div id="filterList">
-            {categories.map((category) => (
-                <div 
-                key={category.category_id} 
-                data-id={category.category_id} 
-                className="filterListItem" 
-                style={{backgroundColor: category.color}}
-                >
-                    {category.category_name}
-                </div>
-            ))}
+            {categories.map((category) => {
+                const isSelected = selected.includes(category.category_id);
+                const style = {
+                    backgroundColor: category.color,
+                    border: isSelected ? "2px solid black" : "2px solid #0000"
+                };
+                return (
+                    <div 
+                    key={category.category_id} 
+                    data-id={category.category_id} 
+                    className="filterListItem" 
+                    style={style}
+                    onClick={handleClick} 
+                    >
+                        {category.category_name}
+                    </div>
+                );
+            })}
         </div>
         </>
     );
