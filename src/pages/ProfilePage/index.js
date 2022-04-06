658640b1ed3556dd340552b2a2bf2577a10a2be1
rxdvd <rxdvd0@gmail.com>
2022-04-07 00:09:20 +0100

    user profile functionality

--- src/pages/ProfilePage/index.js
+++ src/pages/ProfilePage/index.js
@@ -1,19 +1,52 @@
-import React from "react";
-import ProductList from "../../components/ProductList";
+import React, { useEffect, useState } from "react";
+import { useParams, useNavigate } from "react-router-dom";
+import { ProductList, Spinner } from "../../components";
 
 import { Header, NavBar } from "../../layout";
 import "./style.css";
 
 const ProfilePage = () => {
+    const [userInfo, setUserInfo] = useState(null);
+    const [products, setProducts] = useState([]);
+    const { id } = useParams();
+
+    const navigateTo = useNavigate();
+
+    useEffect(async () => {
+        const response = await fetch(`${API_HOST}/users/${id}`);
+        if(response.status === 200) {
+            const json = await response.json();
+            if(json.length){
+                setUserInfo(json[0]);
+            } else {
+                navigateTo("/products");
+            }
+        }
+    }, []);
+
+    useEffect(async () => {
+        const response = await fetch(`${API_HOST}/users/${id}/products`);
+        if(response.status === 200) {
+            const json = await response.json();
+            setProducts(json);
+        }
+    }, []);
+
+    console.log(products);
+
     return (
         <>
         <Header />
-        <main className="profilepage">
-            <h1>Bojin (5)⭐</h1>
-            <hr />
-            <h2>Bojin's Listed Items</h2>
-            <ProductList />
-        </main>
+        { !userInfo ? 
+            <Spinner />
+        :
+            <main className="profilepage">
+                <h1>{`${userInfo.username} (${userInfo.rating}⭐)`}</h1>
+                <hr />
+                <h2>{`${userInfo.username}'${userInfo.username.charAt(userInfo.length - 1) === 's' ? '' : 's'} Listed Items`}</h2>
+                <ProductList productData={products} />
+            </main>
+        }
         <NavBar />
         </>
     );
