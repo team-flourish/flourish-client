5f2571ea6aab62e871985c033e43100741c60381
rxdvd <rxdvd0@gmail.com>
2022-04-06 11:06:20 +0100

    connect login form to api

--- src/components/LogIn/index.js
+++ src/components/LogIn/index.js
@@ -1,77 +1,80 @@
 import React, { useState } from "react";
-// import { useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
-import './style.css'
-import '../style.css'
-
-const handleSubmit = () => {
-    alert("Sign up/in clicked!");
-  }
-
-// const [formData, setFormData] = useState({ username: "", password: "" });
-
-// const handleInput = (e) => {
-//     setError();
-//     return setFormData((prev) => ({
-//       ...prev,
-//       [e.target.name]: e.target.value
-//     }));
-//   };
+import './style.css';
+import '../style.css';
 
 const LogInForm = () => {
+    const [email, setEmail] = useState("");
+    const [passwrd, setPasswrd] = useState("");
 
-  return (
-    <>
-      <form
-        id="form-login"
-        className="make-me-flex-2"
-        onSubmit={handleSubmit}
-        aria-label="login-aria"  
-      >
-         <div className="form-control">
-          <label className= "sign-up-in-field-title" htmlFor="email">Email</label>
-          <input
-            type="email"
-            name="email"
-            // value={formData.password}
-            // onChange={handleInput}
-            placeholder=""
-            required
-            id="email" 
-            className="input-sign-in-up"
-          />
-        </div>
-        <div className="form-control">
-          <label className= "sign-up-in-field-title" htmlFor="email">Password</label>
-          <input
-            type="password"
-            name="password"
-            // value={formData.password}
-            // onChange={handleInput}
-            placeholder=""
-            required
-            id="password" 
-            className="input-sign-in-up"
-          />
-        </div>
-        <input
-          type="submit"
-          className="log-in-now-button"
-          value="Log in"
-        />
-      </form>
-      {/* {error && (
-        <div className="pb-4 text-center" id="error">
-          {error}
-        </div>
-      )}
-      {loading && (
-        <div className="pb-4 text-center" id="loading">
-          Logging in . . .
-        </div>
-      )} */}
-    </>
-  );
+    const navigateTo = useNavigate();
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        let isValid = !!(email && passwrd);
+        if(isValid){
+            const reqBody = {
+                email, passwrd
+            };
+            const response = await fetch(`${API_HOST}/login`, {
+                method: "POST",
+                headers: new Headers({
+                    'Content-Type': 'application/json'
+                }),
+                body: JSON.stringify(reqBody)
+            });
+            if(response.status === 200){
+                console.log("login successful");
+                navigateTo("/products");
+            } else {
+                console.log("login failed");
+            }
+        } else {
+            console.log("missing fields");
+        }
+    };
+
+    return (
+        <>
+            <form
+                id="form-login"
+                className="make-me-flex-2"
+                onSubmit={handleSubmit}
+                aria-label="login-aria"
+            >
+                <div className="form-control">
+                    <label className="sign-up-in-field-title" htmlFor="email">Email</label>
+                    <input
+                        type="email"
+                        name="email"
+                        value={email}
+                        onChange={(e) => { setEmail(e.target.value) }}
+                        required
+                        id="email"
+                        className="input-sign-in-up"
+                    />
+                </div>
+                <div className="form-control">
+                    <label className="sign-up-in-field-title" htmlFor="email">Password</label>
+                    <input
+                        type="password"
+                        name="password"
+                        value={passwrd}
+                        onChange={(e) => { setPasswrd(e.target.value) }}
+                        required
+                        id="password"
+                        className="input-sign-in-up"
+                    />
+                </div>
+                <input
+                    type="submit"
+                    className="log-in-now-button"
+                    value="Log in"
+                />
+            </form>
+        </>
+    );
 };
 
 export default LogInForm;
